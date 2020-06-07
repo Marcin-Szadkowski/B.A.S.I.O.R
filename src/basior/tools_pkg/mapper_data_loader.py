9aa06921f1d1a7aeddb3b62ef784abe3044e8753
szymos <skoczylas9szymon@gmail.com>
2020-06-07 22:18:37 +0200

    Updated tools_pkg

--- src/basior/tools_pkg/mapper_data_loader.py
+++ src/basior/tools_pkg/mapper_data_loader.py
@@ -3,7 +3,10 @@ import os
 
 
 class MapDataLoader(object):
-
+    """
+    Note that all data from: tram_loops.json, edge.json, to_merge.json, to_connect.json
+    propagates to update_graph() in dataloader
+    """
     def __init__(self):
         self.loops = self.get_loop_data()
         self.deleted_edges = self.get_deleted_edges()
@@ -11,8 +14,7 @@ class MapDataLoader(object):
     def append_to_loops_data(self, data):
         for i in range(len(data)):
             self.loops.append(data[i])
-
-        file = open('tram_loops.json.json', )
+        file = open('tram_loops.json', )
         f = json.load(file)
         f['data'] = self.loops
         file.close()
@@ -26,7 +28,7 @@ class MapDataLoader(object):
         f['data'] = self.deleted_edges
         file.close()
 
-    def create_edges_json(self, dict):
+    def create_edges_json(self,dict):
         with open("edges.json", "w") as output:
             json.dump(dict, output)
 
@@ -35,15 +37,32 @@ class MapDataLoader(object):
             json.dump(dict, output)
 
     def get_loop_data(self):
-        file = open('tram_loops.json', )
+        file = open('C:/Users/User_1/PycharmProjects/temporary/tools_pkg/tram_loops.json', )
         data = json.load(file)
         file.close()
 
         return data['data']
 
     def get_deleted_edges(self):
-        file = open('edges.json', )
+        file = open('C:/Users/User_1/PycharmProjects/temporary/tools_pkg/edges.json', )
         data = json.load(file)
         file.close()
+        return data['data']
 
+    def get_to_merge(self):
+        file = open('C:/Users/User_1/PycharmProjects/temporary/tools_pkg/to_merge.json', )
+        data = json.load(file)
+        file.close()
+        return data['data']
+
+    def get_to_merge_preproc(self):
+        file = open('C:/Users/User_1/PycharmProjects/temporary/tools_pkg/to_merge_preproc.json', )
+        data = json.load(file)
+        file.close()
+        return data['data']
+
+    def get_to_connect(self):
+        file = open('C:/Users/User_1/PycharmProjects/temporary/tools_pkg/to_connect.json', )
+        data = json.load(file)
+        file.close()
         return data['data']
